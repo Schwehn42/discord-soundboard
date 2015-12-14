e0af85b5abb16e558a2f80955164352749bf7a1e
Jakob Schwehn <schwehn42@gmail.com>
2015-12-14 22:54:31 +0100

    blah blah tests

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import logging
 import sys
+import asyncio
 
 logging.basicConfig(level=logging.INFO) #basic logging
 
@@ -11,7 +12,6 @@ client = discord.Client()
 creds = open(__file__ + '/../' + 'credentials.txt','r').read() #get creds from file
 creds = creds.split("\n") #remove \n from pw
 client.login(creds[0], creds[1])
-
 @client.event
 def on_message(message):
     if message.content.startswith('!schwehn-exit'):
@@ -23,11 +23,24 @@ def on_message(message):
         for c in channels:
             output = output + c.name + ": " + c.type + "\n"
         client.send_message(message.channel, output)
-    if message.content.startswith('!voice'):
-         voice = yield from client.join_voice_channel(message.channel)
-         player = voice.create_ffmpeg_player('cool.mp3')
-         player.start()
+    if message.content.startswith('!schwehn-join-test'):
+        loop = asyncio.get_event_loop()
+        # Blocking call which returns when the hello_world() coroutine is done
+        loop.run_until_complete(joinVoiceChannel(message.server))
+        loop.close()
 
+
+async def joinVoiceChannel(server):
+    joinChannel = getChannelByName(server, "Kacken")
+    print (joinChannel.name)
+    client.join_voice_channel(joinChannel)
+
+def getChannelByName(server, name):
+    channels = server.channels
+    for c in channels:
+        if c.name == name:
+            return c
+    return -1
 
 @client.event
 def on_ready():
